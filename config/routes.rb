c6bd0f75426dfa836acf8fe4b9d6105d1775becd
kevin collier <collier.kevin.m@gmail.com>
2023-11-03 20:14:29 +0900

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :questions, only: [:show] do
+        resources :answers, only: [:create]
+      end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      resources :locations, only: [:index, :show]
+    end
+  end
 end
