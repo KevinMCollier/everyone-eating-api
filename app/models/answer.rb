52864ee6243ffd79c108bb454070b07ebaca5022
kevin collier <collier.kevin.m@gmail.com>
2023-11-03 16:23:17 +0900

    added validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,7 @@ class Answer < ApplicationRecord
   belongs_to :question
   has_many :answer_locations
   has_many :locations, through: :answer_locations
+
+  validates :response, presence: true
+  validates :question_id, presence: true
 end
